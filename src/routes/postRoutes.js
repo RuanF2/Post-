11639f00db8c0d1f713d62e0d81ce0e827d4a1d1
RuanF2 <ruanfrancelino47@gmail.com>
2026-09-06 21:40:58 -0300

    postRoutes.js atualizado

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 const verificarToken = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/uploadMiddleware');
 
-router.post('/', verificarToken, postController.criarPost );
+router.post('/', verificarToken, upload.single('imagem'), postController.criarPost);
 router.get('/feed', postController.listarFeed);
 router.get('/usuario/:user_id',postController.listarPorUsuario);
 router.get('/:id', postController.buscarPorId);
