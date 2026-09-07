5fe26acf6d8585dfa3adaf2f313327cecd2c0bc2
RuanF2 <ruanfrancelino47@gmail.com>
2026-09-06 21:32:20 -0300

    Middleware de upload pronto!

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -1,3 +1,15 @@
 const multer = require('multer');
 const path = require('path');
 
+const armazenamento = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '..', '..', 'public', 'uploads'))
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    },
+});
+
+const upload = multer({storage: armazenamento});
+
+module.exports = upload;
